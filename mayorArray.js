744cd156058d88b8523607e1ff3ef3bb900ad1e4
Juan Millon <threeinamillon@gmail.com>
2023-05-12 20:20:59 -0500

    todo

---
+++ mayorArray.js
@@ -0,0 +1,28 @@
+console.log()
+function mayorArray(array) {
+    if (array.length === 0) {
+      return null;
+    } else if (array.length === 1) {
+      return array[0];
+    } else {
+      let mayor = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] > mayor) {
+          mayor = array[i];
+        }
+      }
+      let j = 0;
+      while (j < array.length) {
+        console.log(array[j]);
+        j++;
+      }
+      let contadorImpares = 0;
+      do {
+        if (array[j] % 2 !== 0) {
+          contadorImpares++;
+        }
+        j++;
+      } while (j < array.length);
+      return mayor;
+    }
+  }  
